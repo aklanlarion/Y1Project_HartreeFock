a0f8eaa9f191286ad9b0a68501015d0dc4b82f94
KLYYCYNN <120261761+KLYYCYNN@users.noreply.github.com>
2025-06-02 11:20:14 +0100

    added carbon, chlorine atoms

--- atoms.py
+++ atoms.py
@@ -12,6 +12,10 @@ class contracted_gaussians():
         self.coords = coords
         self.Normalisation = (2*alpha/np.pi)**0.75
         self.L = L
+        
+        
+        
+        
 
 def H(coordinates): #hydrogen atom
     H_cg1a = contracted_gaussians(0.3425250914E+01, 0.1543289673E+00, coordinates, [0, 0, 0])
@@ -20,12 +24,55 @@ def H(coordinates): #hydrogen atom
     
     return [[H_cg1a, H_cg1b, H_cg1c]]
 
+
+
+
 def He(coordinates): #helium atom
      He_cg1a = contracted_gaussians(0.6362421394E+01, 0.1543289673E+00, coordinates, [0, 0, 0])
      He_cg1b = contracted_gaussians(0.1158922999E+01, 0.5353281423E+00, coordinates, [0, 0, 0])
      He_cg1c = contracted_gaussians(0.3136497915E+00, 0.4446345422E+00, coordinates, [0, 0, 0])
      
      return  [[He_cg1a, He_cg1b, He_cg1c]]
+ 
+    
+ 
+    
+    
+def C(coordinates): #carbon atom
+    ls, lx, ly, lz = [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]
+    
+    C_1s1 = contracted_gaussians(0.7161683735E+02, 0.1543289673E+00, coordinates, ls)
+    C_1s2 = contracted_gaussians(0.1304509632E+02, 0.5353281423E+00, coordinates, ls)
+    C_1s3 = contracted_gaussians(0.3530512160E+01, 0.4446345422E+00, coordinates, ls)
+    
+    C_2s1 = contracted_gaussians(0.2941249355E+01, -0.9996722919E-01, coordinates, ls)
+    C_2s2 = contracted_gaussians(0.6834830964E+00, 0.3995128261E+00,  coordinates, ls)
+    C_2s3 = contracted_gaussians(0.2222899159E+00, 0.7001154689E+00,  coordinates, ls)
+    
+    C_2p1x = contracted_gaussians(0.2941249355E+01, 0.1559162750E+00, coordinates, lx)
+    C_2p1y = contracted_gaussians(0.2941249355E+01, 0.1559162750E+00, coordinates, ly)
+    C_2p1z = contracted_gaussians(0.2941249355E+01, 0.1559162750E+00, coordinates, lz)
+    
+    C_2p2x = contracted_gaussians(0.6834830964E+00, 0.6076837186E+00, coordinates, lx)
+    C_2p2y = contracted_gaussians(0.6834830964E+00, 0.6076837186E+00, coordinates, ly)
+    C_2p2z = contracted_gaussians(0.6834830964E+00, 0.6076837186E+00, coordinates, lz)
+    
+    C_2p3x = contracted_gaussians(0.2222899159E+00, 0.3919573931E+00, coordinates, lx)
+    C_2p3y = contracted_gaussians(0.2222899159E+00, 0.3919573931E+00, coordinates, ly)
+    C_2p3z = contracted_gaussians(0.2222899159E+00, 0.3919573931E+00, coordinates, lz)
+    
+    C_1s = [C_1s1, C_1s2, C_1s3]
+    C_2s = [C_2s1, C_2s2, C_2s3]
+    C_2px = [C_2p1x, C_2p2x, C_2p3x]
+    C_2py = [C_2p1y, C_2p2y, C_2p3y]
+    C_2pz = [C_2p1z, C_2p2z, C_2p3z]
+    
+    return [C_1s, C_2s, C_2px, C_2py, C_2pz]
+    
+
+
+
+
  
 def O(coordinates): #oxygen atom
     ls, lx, ly, lz = [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]
@@ -57,6 +104,11 @@ def O(coordinates): #oxygen atom
     O_2pz = [O_2p1z, O_2p2z, O_2p3z]
     
     return [O_1s, O_2s, O_2px, O_2py, O_2pz]
+
+
+
+
+
     
 def Na(coordinates): # sodium atom
     Na_1s1 = contracted_gaussians(0.2507724300E+03, 0.1543289673E+00, coordinates, [0, 0, 0])
@@ -105,3 +157,58 @@ def Na(coordinates): # sodium atom
     
     
     return [chi_1s, chi_2s, chi_2px, chi_2py, chi_2pz, chi_3s]
+
+
+
+def Cl(coordinates):  #chlorine atom
+        ls, lx, ly, lz = [0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]
+
+        Cl_1s1 = contracted_gaussians(0.6013456136E+03, 0.1543289673E+00, coordinates, ls)
+        Cl_1s2 = contracted_gaussians(0.1095358542E+03, 0.5353281423E+00, coordinates, ls)
+        Cl_1s3 = contracted_gaussians(0.2964467686E+02, 0.4446345422E+00, coordinates, ls)
+        
+        Cl_2s1 = contracted_gaussians(0.3896041889E+02, -0.9996722919E-01, coordinates, ls)
+        Cl_2s2 = contracted_gaussians(0.9053563477E+01, 0.3995128261E+00,  coordinates, ls)
+        Cl_2s3 = contracted_gaussians(0.2944499834E+01, 0.7001154689E+00,  coordinates, ls)
+        
+        Cl_2p1x = contracted_gaussians(0.3896041889E+02, 0.1559162750E+00, coordinates, lx)
+        Cl_2p1y = contracted_gaussians(0.3896041889E+02, 0.1559162750E+00, coordinates, ly)
+        Cl_2p1z = contracted_gaussians(0.3896041889E+02, 0.1559162750E+00, coordinates, lz)
+        
+        Cl_2p2x = contracted_gaussians(0.9053563477E+01, 0.6076837186E+00, coordinates, lx)
+        Cl_2p2y = contracted_gaussians(0.9053563477E+01, 0.6076837186E+00, coordinates, ly)
+        Cl_2p2z = contracted_gaussians(0.9053563477E+01, 0.6076837186E+00, coordinates, lz)
+        
+        Cl_2p3x = contracted_gaussians(0.2944499834E+01, 0.3919573931E+00, coordinates, lx)
+        Cl_2p3y = contracted_gaussians(0.2944499834E+01, 0.3919573931E+00, coordinates, ly)
+        Cl_2p3z = contracted_gaussians(0.2944499834E+01, 0.3919573931E+00, coordinates, lz)
+        
+        Cl_3s1 = contracted_gaussians(0.2129386495E+01, -0.2196203690E+00, coordinates, ls)
+        Cl_3s2 = contracted_gaussians(0.5940934274E+00, 0.2255954336E+00,  coordinates, ls)
+        Cl_3s3 = contracted_gaussians(0.2325241410E+00, 0.9003984260E+00,  coordinates, ls)
+        
+        Cl_3p1x = contracted_gaussians(0.2129386495E+01, 0.1058760429E-01, coordinates, lx)
+        Cl_3p1y = contracted_gaussians(0.2129386495E+01, 0.1058760429E-01, coordinates, ly)
+        Cl_3p1z = contracted_gaussians(0.2129386495E+01, 0.1058760429E-01, coordinates, lz)
+        
+        Cl_3p2x = contracted_gaussians(0.5940934274E+00, 0.5951670053E+00, coordinates, lx)
+        Cl_3p2y = contracted_gaussians(0.5940934274E+00, 0.5951670053E+00, coordinates, ly)
+        Cl_3p2z = contracted_gaussians(0.5940934274E+00, 0.5951670053E+00, coordinates, lz)
+        
+        Cl_3p3x = contracted_gaussians(0.2325241410E+00, 0.4620010120E+00, coordinates, lx)
+        Cl_3p3y = contracted_gaussians(0.2325241410E+00, 0.4620010120E+00, coordinates, ly)
+        Cl_3p3z = contracted_gaussians(0.2325241410E+00, 0.4620010120E+00, coordinates, lz)
+        
+        
+        Cl_1s = [Cl_1s1, Cl_1s2, Cl_1s3]
+        Cl_2s = [Cl_2s1, Cl_2s2, Cl_2s3]
+        Cl_2px = [Cl_2p1x, Cl_2p2x, Cl_2p3x]
+        Cl_2py = [Cl_2p1y, Cl_2p2y, Cl_2p3y]
+        Cl_2pz = [Cl_2p1z, Cl_2p2z, Cl_2p3z]
+        Cl_3s = [Cl_3s1, Cl_3s2, Cl_3s3]
+        Cl_3px = [Cl_3p1x, Cl_3p2x, Cl_3p3x]
+        Cl_3py = [Cl_3p1y, Cl_3p2y, Cl_3p3y]
+        Cl_3pz = [Cl_3p1z, Cl_3p2z, Cl_3p3z]
+        
+        return  [Cl_1s, Cl_2s, Cl_2px, Cl_2py, Cl_2pz, Cl_3s, Cl_3px, Cl_3py, Cl_3pz]
+        
