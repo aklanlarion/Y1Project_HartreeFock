e91c6dd8ab9135ac23b403ec829a10efb2277fc7
KLYYCYNN <120261761+KLYYCYNN@users.noreply.github.com>
2025-06-02 10:40:54 +0100

    still helium hydride, need to do bigger molecules!

--- algorithm.py
+++ algorithm.py
@@ -5,11 +5,11 @@ The self-consistent field cycle, definition of the bases, and the idea for the i
 https://github.com/nickelandcopper/HartreeFockPythonProgram/blob/main/Hartree_Fock_Program.ipynb
 '''
 
-#importing necessary packages
 import numpy as np
 from scipy import linalg
 import matrices as mat
 import intergrals as integrals
+import atoms
 import matplotlib.pyplot as plt
 
 #---Define the molecule and basis functions---
@@ -21,43 +21,6 @@ class contracted_gaussians():
         self.Normalisation = (2*alpha/np.pi)**0.75
         self.L = L
         
-
-''' Helium is defined below, remove quotation marks to use. 
-atomic_coordinates = [np.array([0,0,0])] #Add the coordinates of each atom, using known bond lengths
-atomic_masses = [2] #Add the masses of each atom, making sure they're in the same order as above
-assert len(atomic_coordinates) == len(atomic_masses)
-
-He_cg1a = contracted_gaussians(0.6362421394E+01, 0.1543289673E+00, atomic_coordinates[0])
-He_cg1b = contracted_gaussians(0.1158922999E+01, 0.5353281423E+00, atomic_coordinates[0])
-He_cg1c = contracted_gaussians(0.3136497915E+00, 0.4446345422E+00, atomic_coordinates[0])
-
-Hes = [He_cg1a, He_cg1b, He_cg1c]
-Slater_bases = [Hes] 
-nelectrons = 2 # Number of electrons in the system
-nbasis = len(Slater_bases) #Number of basis sets
-'''
-''' Helium Hydride
-atomic_coordinates = [np.array([0,0,0]), np.array([0.790*1.88973, 0, 0])]
-atomic_masses = [2, 1]
-assert len(atomic_coordinates) == len(atomic_masses)
-
-He_cg1a = contracted_gaussians(0.6362421394E+01, 0.1543289673E+00, atomic_coordinates[0])
-He_cg1b = contracted_gaussians(0.1158922999E+01, 0.5353281423E+00, atomic_coordinates[0])
-He_cg1c = contracted_gaussians(0.3136497915E+00, 0.4446345422E+00, atomic_coordinates[0])
-H_cg1a = contracted_gaussians(0.3425250914E+01, 0.1543289673E+00, atomic_coordinates[1])
-H_cg1b = contracted_gaussians(0.6239137298E+00, 0.5353281423E+00, atomic_coordinates[1])
-H_cg1c = contracted_gaussians(0.1688554040E+00, 0.4446345422E+00, atomic_coordinates[1])
-
-Hs = [H_cg1a, H_cg1b, H_cg1c]
-Hes = [He_cg1a, He_cg1b, He_cg1c]
-Slater_bases = [Hes, Hs]
-nelectrons = 2
-nbasis = len(Slater_bases)
-
-print(integrals.one_electron_kinetic(Slater_bases[0][0], Slater_bases[1][0]))
-
-'''
-#------
 
 def electron_density(points, density_matrix, Slater_bases):
     '''
@@ -110,53 +73,17 @@ def SCF_cycle(H, V, S_inverse_sqrt):
             i +=1
     return P, F
 
-#---Define the relevant matrices----
-'''
-H = mat.H_core(Slater_bases, atomic_coordinates, atomic_masses, nbasis) #Core hamiltonian
-V = mat.V_ee(Slater_bases, nbasis) #Electron-electron repulsion
-S = mat.Overlap_matrix(Slater_bases, nbasis) #Overlap matrix
-S_inverse_sqrt = linalg.sqrtm(linalg.inv(S))
-T = mat.Kinetic(Slater_bases, nbasis)
-Vne = mat.Nuclear_electron(Slater_bases, atomic_coordinates, atomic_masses, nbasis)
-'''
-'''
-print(H, 'Core hamiltonian \n')
-print(V, 'Two-electron terms \n')
-print(S, 'Overlap \n')
-print(T, 'Kinetic \n')
-print(Vne, 'Nuclear electron \n')
-'''
-#------
 
-#Call the SCF cycle, calculate electronic energy
-'''
-Density_matrix, Fock_matrix = SCF_cycle(H, V, S_inverse_sqrt)
-print(Density_matrix)
-Energy = E_tot(H, Density_matrix, Fock_matrix)
-print(Energy)
-'''
-# --- Graph total energy against bond length
-
-'''
+atomic_masses = [2, 1]
 xval = np.arange(0.01, 3, 0.01)
-x3dval = np.array([[i, 0.0, 0.0] for i in xval])
+x3dval = np.array([[i, 0.0, 0.0] for i in xval]) #smart way to calculate at different seperations
 Energy = np.zeros([len(xval)])
 for i in range(len(xval)):
     points = x3dval[i]
     atomic_coordinates = [np.array([0,0,0]), points]
-    atomic_masses = [2, 1]
     assert len(atomic_coordinates) == len(atomic_masses)
-
-    He_cg1a = contracted_gaussians(0.6362421394E+01, 0.1543289673E+00, atomic_coordinates[0])
-    He_cg1b = contracted_gaussians(0.1158922999E+01, 0.5353281423E+00, atomic_coordinates[0])
-    He_cg1c = contracted_gaussians(0.3136497915E+00, 0.4446345422E+00, atomic_coordinates[0])
-    H_cg1a = contracted_gaussians(0.3425250914E+01, 0.1543289673E+00, atomic_coordinates[1])
-    H_cg1b = contracted_gaussians(0.6239137298E+00, 0.5353281423E+00, atomic_coordinates[1])
-    H_cg1c = contracted_gaussians(0.1688554040E+00, 0.4446345422E+00, atomic_coordinates[1])
-
-    Hs = [H_cg1a, H_cg1b, H_cg1c]
-    Hes = [He_cg1a, He_cg1b, He_cg1c]
-    Slater_bases = [Hes, Hs]
+    
+    Slater_bases = atoms.He(atomic_coordinates[0]) + atoms.H(atomic_coordinates[1])
     nelectrons = 2
     nbasis = len(Slater_bases)
 
@@ -172,7 +99,7 @@ plt.plot(xval, Energy)
 plt.xlabel('Distance, Angstrom')
 plt.ylabel('Energy, Hartrees')
 plt.show()
-'''
+
 
 #---Graph charge density----
 
@@ -183,3 +110,4 @@ plt.xlabel('Distance, Angstrom')
 plt.ylabel('Electron Density (e/Bohr^3)')
 plt.title('Electron Density')
 plt.show()
+
